5fcd29f5fd5a73422bda8a8e118848347d53df8a
Frienchiese <batuhan.yildiz113@gmail.com>
2023-02-09 20:43:45 +0300

    09.02

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,7 +5,7 @@ import ChannelCard from "./ChannelCard";
 const Videos = ({ videos }) => {
   console.log(videos);
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={0.5}>
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={1}>
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
